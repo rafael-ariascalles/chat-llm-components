28ba25f44bb618cff1a9bcd470bcced740a14c32
Rafael Arias <45745870+rafael-ariascalles@users.noreply.github.com>
2023-06-15 01:52:59 +0000

    [code] Implementation of Docker container

--- ui/streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def main():
-    # Set Streamlit app title and header
-    st.title('Chat with your PDF')
-    st.write('Upload your PDF files and ask questions about them. Already uploaded your files? Skip to the chat below.')
